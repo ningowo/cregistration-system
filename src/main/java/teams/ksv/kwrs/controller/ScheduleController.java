6b4aea0176548b2d9d81e45ef5a60fe063f7ce05
ningd <ningding57@gmail.com>
2022-12-01 23:38:02 -0800

    finish schedule API

--- src/main/java/teams/ksv/kwrs/controller/ScheduleController.java
+++ src/main/java/teams/ksv/kwrs/controller/ScheduleController.java
@@ -2,6 +2,7 @@ package teams.ksv.kwrs.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import teams.ksv.kwrs.model.Schedule;
 import teams.ksv.kwrs.service.ScheduleService;
 import teams.ksv.kwrs.vo.CommonResponse;
 import teams.ksv.kwrs.vo.ScheduleRequest;
@@ -20,19 +21,21 @@ public class ScheduleController {
 
     @GetMapping("/get/{sid}")
     public CommonResponse advisorApprove(@RequestBody ScheduleRequest request) {
-
+        Schedule schedule = request.getSchedule();
+        schedule.setStatus(2);
+        scheduleService.update(request.getSchedule(), request.getSid());
         return CommonResponse.createSuccessResult();
     }
 
-    @PostMapping("/update")
-    public CommonResponse updateSchedule(@RequestBody ScheduleRequest request) {
-        try {
-            scheduleService.update(request.getSchedule(), request.getSid());
-            return CommonResponse.createSuccessResult();
-        } catch (Exception e) {
-            e.printStackTrace();
-            return CommonResponse.createFailResult();
-        }
-    }
+//    @PostMapping("/update")
+//    public CommonResponse updateSchedule(@RequestBody ScheduleRequest request) {
+//        try {
+//
+//            return CommonResponse.createSuccessResult();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            return CommonResponse.createFailResult();
+//        }
+//    }
 
 }
